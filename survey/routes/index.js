8d7f0e14dbdccf97f30c56d10abe65ef83a6ee4a
hyohun2684 <hyohun2684@hanmail.net>
2015-12-09 16:53:34 +0900

    add routes

--- survey/routes/index.js
+++ survey/routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send("asd");
+});
+
+router.get('/logout', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send("asd");
+});
+
+router.get('/manage', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send("asd");
+});
+
+router.get('/create', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send("asd");
+});
+
+router.get('/stat/:a', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send(req.params.a);
+});
+
+router.get('/survey/:a', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send(a);
+});
+
+router.get('/dbtest/:a', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send(a);
 });
 
 module.exports = router;
