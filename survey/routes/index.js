a12e0fb285645849fec7101526af286e3695a05e
hyohun2684 <hyohun2684@hanmail.net>
2015-12-10 01:08:58 +0900

    add developing survey create page

--- survey/routes/index.js
+++ survey/routes/index.js
@@ -35,8 +35,11 @@ router.get('/manage', function(req, res, next) {
 });
 
 router.get('/create', function(req, res, next) {
+  res.render('create', { title: 'Create your own survey page' });
+});
+
+router.get('/save', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send("asd");
 });
 
 router.get('/stat/:a', function(req, res, next) {
