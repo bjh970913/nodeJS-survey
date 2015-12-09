b633dffd33ef169a167a09b2a232a58e2fa5412e
hyohun2684 <hyohun2684@hanmail.net>
2015-12-09 21:21:59 +0900

    add mongodb connection

--- survey/routes/index.js
+++ survey/routes/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
+// Connection URL
+var url = 'mongodb://admin:admin@ds053764.mongolab.com:53764/hyohun';
+// Use connect method to connect to the Server
+var db;
 
+MongoClient.connect(url, function(err, db_conn) {
+  assert.equal(null, err);
+  db = db_conn;
+  console.log("Connected correctly to server");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -35,12 +46,15 @@ router.get('/stat/:a', function(req, res, next) {
 
 router.get('/survey/:a', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send(a);
+  res.send("asd");
 });
 
 router.get('/dbtest/:a', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send(a);
+  db.collection('data').findOne({_id:req.params.a}, function(err, doc) {
+    res.send(doc);
+    db.close();
+  });
 });
 
 module.exports = router;
