9147ff7ee698987664aa9ffa5ca118495614c7aa
hyohun2684 <hyohun2684@hanmail.net>
2015-12-14 00:13:58 +0900

    fix link port part add heroku link

--- survey/routes/index.js
+++ survey/routes/index.js
@@ -10,7 +10,7 @@ var shortid = require('shortid');
 
 var DB = require('./db.js')
 
-var serviceUrl = "http://127.0.0.1";
+var serviceUrl = "http://nodesurvey.herokuapp.com/";
 
 //====================================================
 //setting Mongodb
@@ -85,7 +85,7 @@ router.post('/save', check_auth, function(req, res, next) {
   SurveyData.insertOne({user:req.user.username, data:req.body, url:uurl, title:req.body.title, ans:0},
     function (err,result) {
       assert.equal(err, null);
-      res.send('성공적으로 설문을 생성했습니다.<br><a href="'+serviceUrl+':3000/survey/'+uurl+'">LINK</a>링크를 통해 설문에 참여하도록 할 수 있습니다.');
+      res.send('성공적으로 설문을 생성했습니다.<br><a href="'+serviceUrl+'/survey/'+uurl+'">LINK</a>링크를 통해 설문에 참여하도록 할 수 있습니다.');
   });
 });
 
